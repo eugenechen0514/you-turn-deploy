9c0e80ab3cceecbdfab094ceaf4d1f2d7244d9d3
EugeneChen <yujiechen0514@gmail.com>
2018-11-15 16:23:49 +0800

    release beta

---
+++ precache-manifest.e25f7e3c205ceb3870123892c0cc6353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e33ded654133dff071bd",
+    "url": "/static/js/main.e33ded65.chunk.js"
+  },
+  {
+    "revision": "9af22383188fcbb1f7e0",
+    "url": "/static/js/1.9af22383.chunk.js"
+  },
+  {
+    "revision": "e33ded654133dff071bd",
+    "url": "/static/css/main.9cc38d4b.chunk.css"
+  },
+  {
+    "revision": "fd73913878f7275f9afb42be0cfaf10e",
+    "url": "/index.html"
+  }
+];
