c0806cbfa79b2ee3b7e4da72306d090219b36a8e
EugeneChen <yujiechen0514@gmail.com>
2018-11-16 16:29:52 +0800

    test code

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/mymqtt', function (req, res) {
+    res.send(process.env.CLOUDMQTT_URL);
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
