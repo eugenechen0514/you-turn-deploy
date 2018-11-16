07ce1e4d8d03ab3083e6bfa0b72ffbcf1ab29584
EugeneChen <yujiechen0514@gmail.com>
2018-11-16 15:52:43 +0800

    add mqtt proxy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const httpProxy = require('http-proxy');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,23 +20,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+const proxy = httpProxy.createProxyServer({});
 // app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/mqtt', function (req, res) {
+    proxy.ws(req, res, { target: 'wss://aud.ssp.hinet.net:443/mqtt' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
