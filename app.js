adee8df3ba9f2e25fe3219261a3a9b7f82c2272d
EugeneChen <yujiechen0514@gmail.com>
2018-11-16 16:20:37 +0800

    test mqtt

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const httpProxy = require('http-proxy');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,14 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-const proxy = httpProxy.createProxyServer({});
-// app.use('/', indexRouter);
-app.use('/mqtt', function (req, res) {
-    proxy.ws(req, res, { target: 'wss://aud.ssp.hinet.net:443/mqtt' });
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
